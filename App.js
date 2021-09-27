0f334e7d8291709f9f71e7d268d7560028db546e
YogithaManjunath <90307432+YogithaManjunath@users.noreply.github.com>
2021-09-27 20:48:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello  Cognitive Clouds Interns!</h1>
+    </div>
+  );
+}
+
+export default App;
